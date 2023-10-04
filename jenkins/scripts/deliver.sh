63bba3b7af6b216765bc21a62484663e3a5e8600
Addie Pasok <addie.pasok@va.gov>
2023-10-04 11:01:58 -0400

    changed script to echo message instead of running jar file

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar "target/${NAME}-${VERSION}.jar"
+echo 'App has been deployed'
+# java -jar "target/${NAME}-${VERSION}.jar"
